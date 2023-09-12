fc4d64713a871a41f48b14250bcb0134a11b7714
Phelipe------- de Lima <phelipe.nogueira@acct.global>
2023-09-11 22:16:22 -0300

    feat: include storybook

---
+++ packages/docs/src/stories/Button.stories.tsx
@@ -0,0 +1,9 @@
+import { Button } from '@ignite-ui/react'
+import { StoryObj, Meta } from '@storybook/react'
+
+export default {
+title: 'Buttton',
+component: Button
+} as Meta
+
+export const Primary: StoryObj = {}
