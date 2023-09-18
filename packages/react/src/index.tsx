d0aff48c4b09dfe4d83a1c9b5868f3b1adc169b7
Phelipe------- de Lima <phelipe.nogueira@acct.global>
2023-09-18 20:44:21 -0300

    feat: added Box, Text and Heading components

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,35 +1,3 @@
-import { styled } from "./styles/index"
-import { VariantProps } from '@stitches/react'
-
-
-export const Button = styled('button', {
-  fontFamily: '$default',
-  backgroundColor: '$ignite300',
-  borderRadius: '$sm',
-  border: 0,
-  fontWeight: 'bold',
-  color: '$white',
-
-  variants: {
-    size: {
-      small: {
-        fontSize: 14,
-        padding: '$2 $4',
-      },
-      medium: {
-        fontSize: 16,
-        padding: '$3 $6',
-      },
-      big: {
-        fontSize: 16,
-        padding: '$3 $6',
-      },
-    },
-  },
-
-  defaultVariants: {
-    size: 'small',
-  },
-})
-
-export type ButtonProps = VariantProps<typeof Button>
+export * from './components/Box'
+export * from './components/Text'
+export * from './components/Heading'
