247320eaebb6e8b9eb40f333ebbdbc09a7a528d4
Phelipe------- de Lima <phelipe.nogueira@acct.global>
2023-09-28 20:41:16 -0300

    chore: change packages name

--- packages/docs/src/stories/Heading.stories.tsx
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react'
-import { Heading, HeadingProps } from '@ignite-ui/react'
+import { Heading, HeadingProps } from '@call-nog-ui/react'
 
 export default {
     title: 'Typography/Heading',
